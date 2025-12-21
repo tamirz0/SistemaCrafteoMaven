72958be0de6988f1b8eaf327bea7424b2368daac
Mateo Tamir Avella <mateotamir2004@gmail.com>
2025-12-21 00:48:49 -0300

    Refactorizacion: Actualización de la clase Registro a una de tipo Record.

--- src/main/java/sistema_crafteo/logica/Registro.java
+++ src/main/java/sistema_crafteo/logica/Registro.java
@@ -1,47 +1,26 @@
 package sistema_crafteo.logica;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
 import sistema_crafteo.objeto.Item;
 
-public class Registro {
-    private final String item;
-    private final Map<String, Integer> ingredientes;
-    private final String fecha;
+import java.time.LocalDateTime;
+import java.util.Map;
 
-    public Registro(Item item, Map<Item, Integer> ingredientes) {
+public record Registro(Item item, Map<Item, Integer> ingredientes, String fechaHora) {
+    public Registro {
         if (item == null) {
             throw new NullPointerException("Puntero a item nulo");
         }
         if(ingredientes == null) {
             throw new NullPointerException("Puntero a ingredientes nulo");
         }
-        this.item = item.toString();
-        Map<String, Integer> ingredientesString = new HashMap<>();
-        for (Map.Entry<Item, Integer> entrada : ingredientes.entrySet()) {
-            ingredientesString.put(entrada.getKey().toString(), entrada.getValue());
-        }
-        this.ingredientes = ingredientesString;
-        this.fecha = LocalDateTime.now().toString();
     }
-
-    public String getItem() {
-        return item;
-    }
-
-    public Map<String, Integer> getIngredientes() {
-        return ingredientes;
-    }
-
-    public String getFecha() {
-        return fecha;
+    public Registro(Item item, Map<Item, Integer> ingredientes){
+        this(item, ingredientes, LocalDateTime.now().toString());
     }
 
     @Override
-    public String toString() {
-        return "[item=" + item + ", ingredientes=" + ingredientes + ", fecha=" + fecha + "]";
+    public String toString(){
+        return "[item=" + item.toString() + ", ingredientes=" + ingredientes.toString() + ", fecha=" + fechaHora + "]";
     }
 
 }
